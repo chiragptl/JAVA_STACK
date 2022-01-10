be7560e3475d9244ffeb1e76aaa5d094933c16f3
Chirag Patel <ptlraju5520@gmail.com>
2022-01-10 14:44:36 +0530

    Main Class added and in StackDS class datamember added

---
+++ StackDS.java
@@ -0,0 +1,7 @@
+public class StackDS{
+    static final int max = 100; // length of the stack
+    int arr[] = new int[max]; // stack operation perform on this array
+    int top=-1; // stack Top pointer
+
+
+}
