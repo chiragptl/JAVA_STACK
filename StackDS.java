c3f0a9c76bcefc564496c313c73429dd8d7cf2a5
Chirag Patel <ptlraju5520@gmail.com>
2022-01-10 15:11:41 +0530

    added pop operation and isEmpty operation

--- StackDS.java
+++ StackDS.java
@@ -3,5 +3,23 @@ public class StackDS{
     int arr[] = new int[max]; // stack operation perform on this array
     int top=-1; // stack Top pointer
 
+    public void pop(){ 
+        if(!isEmpty()) // check if stack is empty then it's underflow
+        {
+            System.out.println("Stack is underflow");
+        }
+        else{// else stack has elements 
+            System.out.println(arr[top]+" is pop out from the stack");
+            top = top - 1;
+        }
+    }
+    public boolean isEmpty(){
+        if(top < 0){ // check if top is less then 0 then stack is empty
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 
 }
