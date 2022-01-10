b556853b65f7db030f52493da3e56f71de07af09
Karan Doshi <97081306+karan-mobiuso@users.noreply.github.com>
2022-01-10 15:31:22 +0530

    contains push and peek operations

--- Main.java
+++ Main.java
@@ -1,6 +1,56 @@
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
+        //Call static methods to pass the values
         StackDS obj = new StackDS();
+        Scanner sc= new Scanner(System.in);
+        int value=0;
+
+        //menu
+        System.out.println("****************************************************");
+        System.out.println("Enter 1 for Push");
+        System.out.println("Enter 2 for Pop");
+        System.out.println("Enter 3 for Peek");
+        System.out.println("Enter 4 for  Is Empty");
+        System.out.println("****************************************************");
+        System.out.println("Please enter the operation you want to perform");
+        int operation= sc.nextInt();
+
+        switch(operation)
+        {
+            case 1: //for push
+                System.out.println("Please enter number:");
+                value = sc.nextInt();
+                int ans = obj.push(value);
+                if(ans == -1)
+                {
+                    System.out.println("Stack overflowed");
+                }
+                else
+                {
+                    System.out.println(value+" added to stack");
+                }
+                break;
+            case 2: //for pop
+                System.out.println(obj.pop());
+                break;
+            case 3: //for peek
+                int ans = obj.peek();
+                if(ans == -1)
+                {
+                    System.out.println("Stack empty!! Please enter value");
+                }
+                else
+                {
+                    System.out.println(ans+" is at top");
+                }
+            case 4: //for is empty
+                System.out.println(obj.isEmpty());
+                break;
+
+            default:
+                break;
+        }
     }
 }
--- StackDS.java
+++ StackDS.java
@@ -3,5 +3,31 @@ public class StackDS{
     int arr[] = new int[max]; // stack operation perform on this array
     int top=-1; // stack Top pointer
 
+    public int push(int val)
+    {
+        if(arr.length()==max)
+        {
+            return -1;
+        }
+        else
+        {
+            arr[top+1]=val;
+            top++;
+            return 1;
+        }
+    }
+
+    public int peek()
+    {
+        if(top == -1)
+        {
+            return -1;
+        }
+        else
+        {
+            return arr[top];
+        }
+    }
+
 
 }
